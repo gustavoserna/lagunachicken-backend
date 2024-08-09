0fdf7ad19b863aced5cdfc91458f5b8d699a8253
Gustavo Serna <gustavoserna@MacBook-Air-de-Gustavo.local>
2024-08-09 13:14:31 -0600

    port change

--- src/main/java/com/digitaul/lagunachicken/utility/MailUtility.java
+++ src/main/java/com/digitaul/lagunachicken/utility/MailUtility.java
@@ -21,7 +21,7 @@ public class MailUtility {
     public static void sendEmail (MailDTO mailDTO) {
         Properties props = new Properties();
         props.put("mail.smtp.host", "mail.lagunachicken.mx");
-        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.port", "465");
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
 
